bb05ffc75f3bc7b3351eb48e6c83e942c08d00d9
sccnick <sccnick@hotmail.com>
2015-08-28 01:44:46 +0700

    1.570610613 lab 1-2

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -8,14 +8,15 @@ namespace Lab1_2
 {
     class BubbleSortFunction
     {
-        static void Main(string[] args)
+        public static int[] inp()
+        {
+            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+            return input;
+        }
+
+        public static void pro(int[] input)
         {
             bool flag = true;
-
-            // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
             while (flag)
             {
                 flag = false;
@@ -27,17 +28,26 @@ namespace Lab1_2
                         input[i] = input[i + 1];
                         input[i + 1] = temp;
                         flag = true;
+                        }
                     }
                 }
             }
 
-            // output
-            for (int i = 0; i != input.Length; i++)
+            public static void outp(int[] input)
             {
-                Console.Write(input[i]);
-                Console.Write(" ");
+                for (int i = 0; i != input.Length; i++)
+                {
+                    Console.Write(input[i]);
+                    Console.Write(" ");
+                }
+                Console.ReadKey();
             }
-            Console.ReadKey();
-        }
-    }
+        
+            static void Main(string[] args)
+            {
+                int[] input = inp();
+                pro(input);
+                outp(input);
+            }
+     }
 }
